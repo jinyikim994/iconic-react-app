7afcfa471ac5e4b59a60423c01fffe4e8a452c93
jinyikim <https://github.com/jinyikim994>
2025-09-10 14:40:21 +0900

    패키지 수정

---
+++ README.md
@@ -0,0 +1,15 @@
+# ICONIC CREATIVE STUDIEO REACT VER
+
+이 프로젝트는 React 프레임워크를 활용하여 하나의 웹사이트를 제작하며, React의 가장 기본적인 구조를 이해하고 학습하는 데 목적이 있습니다.
+
+목표
+1. React의 기본 구성 요소와 구조를 파악
+2. 간단한 포트폴리오 사이트 구현을 통해 React 실습
+
+## 사용 스택
+- React.js: 컴포넌트 기반 UI 구축 및 상태 관리
+- React Router DOM: 페이지 간 라우팅 처리
+
+### 완성작 보기 
+URL : 
+
---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Main from "./components/pages/Main";
+import Artists from "./components/pages/Artists";
+import PhotographerView from "./components/includes/PhotographerView"; 
+import PhotographerViewAd from "./components/includes/PhotographerViewAd"; 
+import PhotographerPersonalAll from "./components/includes/PhotographerViewPersonal"; 
+import StylistView from "./components/includes/StylistView"; 
+import FashionView from "./components/includes/FashionView"; 
+import BeautyView from "./components/includes/BeautyView"; 
+import Production from "./components/pages/Production";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Main Page */}
+        <Route path="/" element={<Main />} />
+        {/* Sub Page */}
+        <Route path="/Artists" element={<Artists />} />
+        <Route path="/PhotographerView" element={<PhotographerView />} />
+        <Route path="/PhotographerViewAd" element={<PhotographerViewAd/>} />
+        <Route path="/PhotographerViewPersonal" element={<PhotographerPersonalAll/>} />
+        <Route path="/StylistView" element={<StylistView />} />
+        <Route path="/FashionView" element={<FashionView />} />
+        <Route path="/BeautyView" element={<BeautyView />} />
+        <Route path="/Production" element={<Production />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
